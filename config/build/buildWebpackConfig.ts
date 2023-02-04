e970c152722487b8579bb5f4cebef137948290e2
igor <igor.danilov1824@gmail.com>
2023-02-04 15:07:02 +0200

    lesson 3 is over

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import webpack from "webpack";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {BuildOptions} from "./types/config";
+import {buildDevServer} from "./buildDevServer";
+
+export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
+  const { paths, mode, isDev } = options
+
+  return {
+      mode: mode,
+      entry: paths.entry,
+      output: {
+          path: paths.build,
+          filename: "[name].[contenthash].js",
+          clean: true,
+      },
+      plugins: buildPlugins(options),
+      module: {
+          rules: buildLoaders()
+      },
+      resolve: buildResolvers(),
+      devtool: isDev ? 'inline-source-map' : undefined,
+      devServer: isDev ? buildDevServer(options) : undefined
+  }
+}
